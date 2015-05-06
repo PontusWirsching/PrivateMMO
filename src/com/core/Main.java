779a3ce8249a6874ab0668ec6adc2723cdbabda3
PontusWirsching <flash13records@gmail.com>
2015-05-06 22:47:52 +0200

    Test commit
    Testing

---
+++ src/com/core/Main.java
@@ -0,0 +1,23 @@
+package com.core;
+
+import com.engine.LEngine;
+
+public class Main extends LEngine {
+
+	public Main(int width, int height, String title) {
+		super(width, height, title);
+		
+		start();
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+}
---
+++ src/com/core/Run.java
@@ -0,0 +1,19 @@
+package com.core;
+
+
+/**
+ * 
+ * @author Pontus Wirsching
+ * @since 2015-05-06
+ */
+public class Run {
+	
+	public static final String TITLE = "Untitled";
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+
+	public static void main(String[] args) {
+		new Main(WIDTH, HEIGHT, TITLE);
+	}
+
+}
